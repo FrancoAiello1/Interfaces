32d2f76218f1dc22373f0f0bf9fe0978caee9a51
Franco Aiello <franaiello2@gmail.com>
2021-10-29 17:01:29 -0300

    Agrega animacion de muerte y fin de juego, agrega animacion al colectar moneda

--- Entregable3/js/script.js
+++ Entregable3/js/script.js
@@ -1,10 +1,15 @@
 
 let puntajeMax = 5;
 let tiempo = 0;
+let tocaTijera = false;
+let detectar;
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    //Al cargarse el html por completo esconde usando JQuery los elementos no necesarios en el momento.
     $("#game").hide();
     $("#ganador").hide();
+    $("#perdedor").hide();
+    $("#explota").hide();
 });
 
 document.onkeydown = function (event) {
@@ -23,7 +28,7 @@ document.onkeydown = function (event) {
 
 
 function jump() {
-    //Agrega la clase gbjump a el personaje, que contiene la aniacion para el mismo, luego la remueve despues de 1 segundo.
+    //Agrega la clase gbjump a el personaje, que contiene la animacion para el mismo, luego la remueve despues de 1 segundo.
     document.getElementById("gb").classList.add("gbjump");
     let agarro = false;
     setTimeout(() => {
@@ -37,20 +42,53 @@ function jump() {
 function agarroMoneda() {
     //Chequea si el personaje se acerco lo suficiente a la moneda como para haberla agarrado, si es así remueve la misma
     //y suma un punto.
+    //Usando JQuery muestra y oculta la animacion de colectar la moneda
     let distance = document.getElementById("moneda").getBoundingClientRect().x - document.getElementById("gb").getBoundingClientRect().x;
     if (distance <= 321 && distance > 0) {
-        //document.getElementById("moneda").classList.add("explode-coin"); ANIMACION DE EXPLOTAR MONEDA, VER COMO HACER Q ANDE NO PUDE xD 
+
         setTimeout(() => {
             document.getElementById("moneda").classList.remove("move-coin");
+            
+            $("#explota").show();
+            setTimeout(() => {
+                $("#explota").hide();
+                document.getElementById("div-moneda").classList.remove("div-moneda");
+            }, 1000);
             console.log("aca");
-
-            document.getElementById("div-moneda").classList.remove("div-moneda");
-
         }, distance)
 
         sumarPuntos();
 
     }
+}
+
+function chocoTijera() {
+    //Con un booleano y las posiciones de los elementos en pantalla verifica si estos dos ultimos 
+    //se tocaron y ejecuta la funcion finJuego()
+
+    let hitboxPanaIzq = document.getElementById("gb").getBoundingClientRect().left;
+    let hitboxPanaTop = document.getElementById("gb").getBoundingClientRect().top;
+    let hitboxTijera = document.getElementById("scissors").getBoundingClientRect().left;
+
+    if (!tocaTijera && parseInt(hitboxTijera) - parseInt(hitboxPanaIzq) <= 0 && parseInt(hitboxTijera) - parseInt(hitboxPanaIzq) > -10 && parseInt(hitboxPanaTop) >= 540) {
+        tocaTijera = true;
+    }
+    if (tocaTijera) {
+        finJuego();
+    }
+}
+
+function finJuego() {
+    //Borra el setInterval que detecta si te chocaste la tijera, agrega la clase con la animacion de muerte
+    //y muestra un mensaje al usuario
+    clearInterval(detectar);
+    document.getElementById("gb").classList.add("gbdead");
+    document.getElementById("div-moneda").classList.remove("div-moneda");
+    setTimeout(() => {
+        $("#game").hide();
+        $("#perdedor").show();
+    }, 1000);
+
 }
 
 function sumarPuntos() {
@@ -66,11 +104,13 @@ function sumarPuntos() {
 }
 
 function ganador() {
+    //Usando JQuery oculta el html del juego y muestra el aviso de ganador
     $("#game").hide();
     $("#ganador").show();
 }
 
 function background2() {
+    //Agrega la clase con diferente imagen a cada capa del escenario 
     console.log("yes");
     document.getElementById("capa1").classList.add("back2-1");
     document.getElementById("capa2").classList.add("back2-2");
@@ -83,6 +123,7 @@ function background2() {
 }
 
 function background1() {
+    //Agrega la clase con diferente imagen a cada capa del escenario 
     document.getElementById("capa1").classList.remove("back2-1");
     document.getElementById("capa2").classList.remove("back2-2");
     document.getElementById("capa3").classList.remove("back2-3");
@@ -94,7 +135,9 @@ function background1() {
 }
 
 function play() {
+    //Usando JQuery muestra el juego y oculta las instrucciones, agrega un setInterval que pregunta 
+    //por la colision de las tijeras y el player model.
     $("#game").show();
     $("#instrucciones").hide();
-
+    detectar = setInterval(chocoTijera, 0);
 }
